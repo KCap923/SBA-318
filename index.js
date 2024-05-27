6b0e54ce4232b0d7a309821229f7847b20d29a42
Krystle Capuano <krystlecapuano@gmail.com>
2024-05-27 18:40:09 -0400

    "Middleware"

--- index.js
+++ index.js
@@ -1,23 +1,68 @@
 const express = require ("express");
+const { title } = require("process");
 const app = express ();
 const PORT = 4000; 
-
-// const usersRouter = require('./routes/books.js');
-// const postsRouter = require('./routes/members.js');
-// const usersRouter = require('./routes/rentals.js');
-// const error = require('./utilities/error.js');
-
+const morgan = require("morgan");
 
 // Template Engine
 app.set("view engine", "ejs");
 
 
+// Middleware
+app.use((req, res, next) => {
+console.log("new request made:");
+console.log("host: ", req.hostname);
+console.log("path: ", req.path);
+console.log("method: ", req.method);
+  next();
+});
+
+//Second Middleware
+app.use(morgan("dev"));
+// Console logs ----->
+// Server is running on port: 4000
+// new request made:
+// host:  localhost
+// path:  /
+// method:  GET
+// Hello
+// GET / 304 9.577 ms - -
+// new request made:
+// host:  localhost
+// path:  /books
+// method:  GET
+// Books
+// GET /books 304 1.800 ms - -
 
 
+//
 app.get("/", (req, res) => {
   console.log("Hello")
   res.render("index");
 });
+
+app.get("/books", (req, res) => {
+  console.log("Books")
+  res.render("books");
+});
+
+app.get("/members", (req, res) => {
+  console.log("Members")
+  res.render("members");
+});
+
+app.get("/rentals", (req, res) => {
+  console.log("Rentals")
+  res.render("rentals");
+});
+
+
+
+
+
+
+
+
 
 
 
@@ -38,17 +83,15 @@ app.get("/", (req, res) => {
 
 
 
-
-
-
-
-
-
-
 // app.use("/books", usersRouter);
 // app.use("/members", usersRouter);
 // app.use("/rentals", usersRouter);
 
+
+//404 Page
+app.use((req, res) => {
+  res.status(404).render("404", {title: "404"});
+});
 
 
 app.listen(PORT, () => {
--- routes/books.js
+++ routes/books.js
@@ -1,3 +1,91 @@
 const express = require('express');
 const router = express.Router();
 const books = require('../data/books');
+
+
+
+// GET route 
+router.get('/', (req, res) => {
+  const links = [
+    {
+      href: 'books/:books',
+      rel: ':id',
+      type: 'GET',
+    },
+  ];
+
+  res.json({ books });
+});
+
+
+
+router.get('/:id', (req, res, next) => {
+  const books = books.find((p) => p.id == req.params.id);
+
+  const links = [
+    {
+      href: `/${req.params.id}`,
+      rel: '',
+      type: 'PATCH',
+    },
+    {
+      href: `/${req.params.id}`,
+      rel: '',
+      type: 'DELETE',
+    },
+  ];
+
+  if (books) res.json({ books, links });
+  else next();
+});
+
+// //Creating a Post (POST)
+// router.post('/', (req, res) => {
+//   // Within the POST request route, we create a new
+//   // user with the data given by the client.
+//   // We should also do some more robust validation here,
+//   // but this is just an example for now.
+//   if (req.body.userId && req.body.title && req.body.content) {
+//     const post = {
+//       id: posts[posts.length - 1].id + 1,
+//       userId: req.body.userId,
+//       title: req.body.title,
+//       content: req.body.content,
+//     };
+
+//     posts.push(post);
+//     res.json(posts[posts.length - 1]);
+//   } else next(error(400, 'Insufficient Data'));
+// });
+
+// router.patch('/:id', (req, res) => {
+//   // Within the PATCH request route, we allow the client
+//   // to make changes to an existing user in the database.
+//   const post = posts.find((p, i) => {
+//     if (p.id == req.params.id) {
+//       // iterating through the user object and updating each property with the data that was sent by the client
+//       for (const key in req.body) {
+//         posts[i][key] = req.body[key];
+//       }
+//       return true;
+//     }
+//   });
+
+//   if (post) res.json(post);
+//   else next();
+// });
+
+// router.delete('/:id', (req, res) => {
+//   // The DELETE request route simply removes a resource.
+//   const post = posts.find((p, i) => {
+//     if (p.id == req.params.id) {
+//       posts.splice(i, 1);
+//       return true;
+//     }
+//   });
+
+//   if (post) res.json(post);
+//   else next();
+// });
+
+module.exports = router;
